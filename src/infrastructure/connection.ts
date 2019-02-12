434e9b797e7b4cf2fa2d241246f53b8cab1eb2c2
sts-msys <stsiwo@gmail.com>
2019-02-11 16:59:12 -0800

    configure dotenv with typescript and use in connection file

--- src/infrastructure/connection.ts
+++ src/infrastructure/connection.ts
@@ -1,12 +1,21 @@
+import "../env";
 import { Sequelize } from 'sequelize-typescript';
 
+/**
+ * since process.env.xxx return string or undefined (union type)
+ * so need type gaurd like "as string" or "<string>"
+ **/
+const dbName: string = process.env.DB_NAME_DEV as string;
+const dbUserName: string = process.env.DB_USER as string;
+const dbPassword: string = process.env.DB_PASSWORD as string;
+
 const sequelize =  new Sequelize({
-  database: 'vocabs',
+  database: dbName, 
   dialect: 'postgres',
-  username: 'postgres',
-  password: 'sts1551iwo',
+  username: dbUserName,
+  password: dbPassword, 
   modelPaths: [
-    __dirname + '/Entities',
+    __dirname + '/Entities'
   ]
 });
 
